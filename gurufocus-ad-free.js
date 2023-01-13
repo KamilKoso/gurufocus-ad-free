2506568f8ddbe1afee4eeca0c7969330514e4011
Kamil Kosobudzki <kamilkosobudzki1@gmail.com>
2023-01-13 16:57:08 +0100

    Rewritten with MutationObserver instead of setInterval's, fixed missing scroll bug

--- gurufocus-ad-free.js
+++ gurufocus-ad-free.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         gurufocus ad-free
 // @namespace    http://tampermonkey.net/
-// @version      1
+// @version      1.1
 // @description  Remove annoying ads from gurufocus :)
 // @author       https://github.com/KamilKoso
 // @match        https://www.gurufocus.com/*
@@ -10,38 +10,34 @@
 // @license      MIT
 // ==/UserScript==
 
+const observer = new MutationObserver(clearPage);
 
 // This cookie is incremented each refresh, if value reaches 6 big ad shows and site is not being fully loaded
 const pwcouCookie = {
-    name: 'pwcou',
-    domain: '.gurufocus.com',
-    desiredValue: -9999,
-    expirationDays: 30
-}
-
-// add here ads that are being displayed only once
-const oneTimeShowingAds = ['section#notification-bar', 'div#colorbox', 'div#cboxOverlay', 'div.ad-image-container'];
+  name: "pwcou",
+  domain: ".gurufocus.com",
+  desiredValue: 0,
+  expirationDays: 30,
+};
 
 // add here ads that are being displayed repeatedly
-const repeatedlyShowingAds = ['div.el-dialog__wrapper', 'div.v-modal', 'div.paywall-shadow', 'div#business-description-non-logged'];
+const adsToClear = ["div.el-dialog__wrapper", "div.v-modal", "div.paywall-shadow", "div#business-description-non-logged", "section#notification-bar", "div#colorbox", "div#cboxOverlay", "div.ad-image-container"];
 
-function clearPage(itemsToClear) {
-    let elems = document.querySelectorAll(itemsToClear);
-    elems.forEach(elem => {
-        elem.remove();
-    });
+function clearPage() {
+  let elems = document.querySelectorAll(adsToClear);
+  setCookie(pwcouCookie.name, pwcouCookie.desiredValue, pwcouCookie.domain, pwcouCookie.expirationDays);
+  elems.forEach((elem) => {
+    elem.remove();
+  });
 }
 
 function setCookie(cName, cValue, cDomain, expDays) {
-     let date = new Date();
-     date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
-     const expires = "expires=" + date.toUTCString();
-     document.cookie = cName + "=" + cValue + "; " + expires + "; domain=" + cDomain +"; path=/";
+  let date = new Date();
+  date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = cName + "=" + cValue + "; " + expires + "; domain=" + cDomain + "; path=/";
 }
 
-(function() {
-     setCookie(pwcouCookie.name, pwcouCookie.desiredValue, pwcouCookie.domain, pwcouCookie.expirationDays);
-     setTimeout(() => {clearPage(oneTimeShowingAds)}, 500);
-     setInterval(() => {clearPage(repeatedlyShowingAds)}, 500);
+(function () {
+  observer.observe(document.body, { childList: true });
 })();
-
